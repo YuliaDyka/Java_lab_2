16d8e7f3816e6e42270b24500960cd6905c7e703
YuliaDyka <handsmade.yulia@gmail.com>
2022-04-09 21:54:45 +0300

    Implemented Lab2

---
+++ src/com/company/FoodProcessor.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class FoodProcessor extends KitchenAppliance{
+    public FoodProcessor(String name, int price, int power){
+        this.name = name;
+        this.price = price;
+        this.power = power;
+    }
+}
---
+++ src/com/company/Oven.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Oven extends KitchenAppliance{
+    public Oven (String name, int price, int power){
+        this.name = name;
+        this.price = price;
+        this.power = power;
+    }
+}
