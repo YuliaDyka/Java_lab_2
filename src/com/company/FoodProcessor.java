c08b528b487c745ad240c35ed4d96d92d2148e82
YuliaDyka <handsmade.yulia@gmail.com>
2022-04-09 21:49:23 +0300

    Revert "Implemented code for Lab 2"
    This reverts commit becd92f83f445fe5ece4f258a2dde26352a77b28.

--- src/com/company/FoodProcessor.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class FoodProcessor extends KitchenAppliance{
-    public FoodProcessor(String name, int price, int power){
-        this.name = name;
-        this.price = price;
-        this.power = power;
-    }
-}
--- src/com/company/Oven.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Oven extends KitchenAppliance{
-    public Oven (String name, int price, int power){
-        this.name = name;
-        this.price = price;
-        this.power = power;
-    }
-}
